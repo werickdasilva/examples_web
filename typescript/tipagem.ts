fd43d7bc61fea2a3b7fbc54004c86c1245c5b10f
Werick da Silva <werick.s.santana34@gmail.com>
2023-01-07 20:17:27 -0300

    feat tipagem em função

--- typescript/tipagem.ts
+++ typescript/tipagem.ts
@@ -24,3 +24,22 @@ let tipo_de_composicao: MeuTipo = 'Vai ser do tipo string';
 // outro jeito de usar o type
 type ValoresQuePoderaoSerUsado = 'Um' | 'Dois' | 'Três'
 let valorUsado: ValoresQuePoderaoSerUsado  = "Três";
+
+// tipagem em função 
+function tipagemEmFuncao(textos: string | string[] ) {
+    // diz que o parametro que vai ter uma string ou um vetor de string
+}
+
+// misturando interface e funcão 
+interface Tamanho {
+    largura: number,
+    altura: number,
+}
+
+function somaDosPontos(tamanho: Tamanho) {
+    const soma = tamanho.largura * tamanho.altura
+    console.log(`A Some entre ${tamanho.largura}X${tamanho.altura} é ${soma}`)
+}
+
+const tamanho = { largura: 20, altura: 35}
+somaDosPontos(tamanho)
